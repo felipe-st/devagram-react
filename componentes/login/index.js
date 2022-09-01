b2e8fe8ac0a49c5b34ffea7e38d2ec8a671435a6
Felipe Taleires <jfelipetaleires@hotmail.com>
2022-08-31 21:30:08 -0300

    Estrutura da Página de Login

---
+++ componentes/login/index.js
@@ -0,0 +1,55 @@
+import InputPublico from "../inputPublico";
+import Botao from "../botao";
+import Image from "next/image";
+import imagemEnvelope from "../../public/imagens/envelope.svg";
+import imagemChave from "../../public/imagens/chave.svg";
+import imagemLogo from "../../public/imagens/logo.svg";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
+    return (
+        <section className={`paginaLogin paginaPublica`}>
+            <div className="logoContainer">
+                <Image
+                    src={imagemLogo}
+                    alt='logotipo'
+                    layout='fill'
+                />
+            </div>
+            <div className="conteudoPaginaPublica">
+                <form>
+                    <InputPublico 
+                        imagem={imagemEnvelope}
+                        texto='E-mail'
+                        tipo='email'
+                        aoAlterarValor={e => setEmail(e.target.value)}
+                        valor={email}
+                    />
+
+                    <InputPublico 
+                        imagem={imagemChave}
+                        texto='Senha'
+                        tipo='password'
+                        aoAlterarValor={e => setSenha(e.target.value)}
+                        valor={senha}
+                    />
+
+                    <Botao 
+                        texto='Login'
+                        tipo='submit'
+                        desabilitado={false}
+                    />
+                </form>
+
+                <div className="rodapePaginaPublica">
+                    <p>Não possui uma conta?</p>
+                    <Link href='/cadastro'>Faça seu cadastro agora/</Link>
+                </div>
+            </div>
+        </section>
+    );
+}
