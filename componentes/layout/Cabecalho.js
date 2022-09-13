a5a68e857b5fd147a7b5632d9e448f3fc66d08c1
Felipe Taleires <jfelipetaleires@hotmail.com>
2022-09-13 08:27:06 -0300

    Barra de pesquisa

--- componentes/layout/Cabecalho.js
+++ componentes/layout/Cabecalho.js
@@ -1,9 +1,24 @@
 import Image from "next/image";
+import { useState } from "react";
 import logoHorizontalImg from "../../public/imagens/logoHorizontal.svg";
 import imagemLupa from "../../public/imagens/lupa.svg";
 import Navegacao from "./Navegacao";
+import ResultadoPesquisa from "./ResultadoPesquisa";
 
 export default function Cabecalho() {
+    const [resultadoPesquisa, setResultadoPesquisa] = useState([]);
+    const [termoPesquisado, setTermoPesquisado] = useState([]);
+
+    const aoClicarResultadoDaPesquisa = (id) => {
+        console.log('aoClicarResultadoDaPesquisa', {id});
+
+        if (termoPesquisado.length < 3) {
+            return;
+        }
+
+        setRe
+    }
+
     return (
         <header className='cabecalhoPrincipal'>
             <div className="conteudoCabecalhoPrincipal">
@@ -25,13 +40,29 @@ export default function Cabecalho() {
                     <input
                         type='text'
                         placeholder='Pesquisar'
-                        value={''}
-                        onChange={() => console.log('pesquisando')}
+                        value={termoPesquisado}
+                        onChange={(e) => setTermoPesquisado(e.target.value)}
                     />
                 </div>
 
                 <Navegacao className='desktop' />
             </div>
+
+            {resultadoPesquisa.length > 0 && (
+                <div className="resultadoPesquisaContainer">
+                    {resultadoPesquisa.map(r => (
+                        <ResultadoPesquisa
+                            avatar={r.avatar}
+                            nome={r.nome}
+                            email={r.email}
+                            key={r._id}
+                            id={r._id}
+                            onClick={aoClicarResultadoDaPesquisa}
+                        />
+                    ))}
+                </div>
+            )}
+
         </header>
     )
 }
